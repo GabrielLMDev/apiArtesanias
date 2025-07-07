e9b4d1fead1263bb7a1a72229200258f13822982
GabrielLMDev <gabriellm.contacto@gmail.com>
2025-07-07 02:02:16 -0600

    2.1 BETA

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,8 +1,24 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./sdk/adminsdk-main.json');
+require('dotenv').config();
+
+/* admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+}); */
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FB_PROJECT_ID,
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FB_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FB_UNIVERSE_DOMAIN
+  }),
 });
 
 const db = admin.firestore();
