fa087c1bee1571837f11cc116e9363a9936e9024
GabrielLMDev <gabriellm.contacto@gmail.com>
2025-07-10 02:52:59 -0600

    3.2 BETA

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 //CORS solo desde frontend
 app.use(cors({
     /* origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501', 'https://tusitio.com'], */
-    origin: ['*'],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
